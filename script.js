0b47e55021ef437d2727dccb8ba28b64f81ea11e
holutunde <holutunde99@gmail.com>
2021-05-21 15:03:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+convertFahrToCelcius = (F) => {
+   if(Array.isArray(F) && typeof(F) === "object"){
+       let output = JSON.stringify(F)
+       console.log(`${output} is not a valid number but a/an array`)
+   }else if(!Array.isArray(F) && typeof(F) === "object"){
+        let value = JSON.stringify(F)
+        console.log (`${(value)} is not a valid number but a/an object`)
+   }else{
+   let val = Number(F)
+      const ans = ((val) - 32) * 5/9; 
+      console.log (`${ans.toFixed(4)}`)  
+  } 
+}
+
+convertFahrToCelcius(0)
+convertFahrToCelcius("0")
+convertFahrToCelcius([1,2,3])
+convertFahrToCelcius({temp: 0})
